f51c0ff20eaee8ec9183c0dd9d10566ad15e4a86
crownbugs <crownbugs@gmail.com>
2026-05-01 00:58:55 -0400

    Adding more

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-project
+# Welcome to your Lovable project
+
+TODO: Document your project here
--- client_backup/src/lib/authUtils.ts
+++
@@ -1,4 +0,0 @@
-// Referenced from javascript_log_in_with_replit blueprint
-export function isUnauthorizedError(error: Error): boolean {
-  return /^401: .*Unauthorized/.test(error.message);
-}
