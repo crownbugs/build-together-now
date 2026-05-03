17a85742fedaaf8abd515600fb00c0c1604f1d85
crownbugs <crownbugs@gmail.com>
2026-05-03 18:00:29 -0400

    Update Landing.tsx

--- client/src/pages/Landing.tsx
+++ client/src/pages/Landing.tsx
@@ -2,7 +2,8 @@ import { Button } from "@/components/ui/button";
 import { Code2, Gamepad2, Users, Zap } from "lucide-react";
 // Change the old import to this:
 // This assumes Landing.tsx is in client/src/pages/
-import heroImage from "../assets/Game_engine_editor_interface_23dc5ed5-BLofF1zR.png";
+import heroImage from "@/assets/Game_engine_editor_interface_23dc5ed5-BLofFlzR.png";
+
 
 
 
